60d4b0ae55a8620d55349c346d749bbbf3b34985
Ana <annageld508@gmail.com>
2023-06-04 19:01:17 +0400

    Created Navigation And vans Page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import "./server";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Link to="/" className="header-logo">
+        <h1 className="header-logo">#VANLIFE</h1>
+      </Link>
+      <nav className="nav-menu">
+        <Link className="nav-link" to="/host">
+          Host
+        </Link>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+        <Link className="nav-link" to="/vans">
+          Vans
+        </Link>
+      </nav>
+    </header>
+  );
+}
